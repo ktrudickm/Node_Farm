46089fb067fdf6c909f4a11f8d5156b6153084e7
Kendall <ktrudickm@gmail.com>
2022-12-07 15:09:54 -0800

    Rendered all products to overview page using product mapping

--- index.js
+++ index.js
@@ -38,22 +38,55 @@ const url = require('url');
 /////////////////////////////
 // SERVER Building
 
+const replaceTemplate = (temp, prod) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, prod.productName);
+    output = output.replace(/{%EMOJI%}/g, prod.image);
+    output = output.replace(/{%NUTRIENTS%}/g, prod.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, prod.quantity);
+    output = output.replace(/{%PRICE%}/g, prod.price);
+    output = output.replace(/{%PRICE%}/g, prod.price);
+    output = output.replace(/{%FROM%}/g, prod.from);
+    output = output.replace(/{%ID%}/g, prod.id);
+
+    if (!prod.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+}
+
+// Note: Can do synchronous version of readFile here because we are in the top level code and this will only run once
+// in the beginning when we load up the application. (Cannot do this in the http.createServer() b/c it is called each time there is a request causing code blocking.)
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
     // Response that we are sending back from the server: 
-
     const pathName = req.url;
 
+    // Overveiw Page
     if (pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, { 'Content-type': 'text/html'});
+
+        // Need to join otherwise product cards are in array format
+        const cardsHTML = dataObj.map(prod => replaceTemplate(tempCard, prod)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+
+        res.end(output);
+
+    // Product Page
     } else if (pathName === '/product'){
         res.end('This is the PRODUCT');
+
+    // API
     } else if (pathName === '/api'){
         res.writeHead(200, { 'Content-type': 'application/json'});
         res.end(data);
+
+    // Not Found
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
